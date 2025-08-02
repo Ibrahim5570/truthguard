391516b2d2fdb409e60623ccb3a801850c14083b
Ibrahim Abdullah <8215@cch.edu.pk>
2025-08-02 16:06:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -228,7 +228,7 @@ def main():
         Found a discrepancy and want to help fix it? Click "✅ Correct" or "❌ Incorrect" to help TruthGuard learn from your knowledge
         and Click 'Retrain Model to have the Model Learn the error and to see some fun balloons!'
         """)
-        st.markdown('<h1 class="header-text; color: #ff0000;">PRONE TO MISTAKES! PLEASE VERIFY FROM OTHER SOURCES!!!<br></h1>',unsafe_allow_html=True)
+    st.markdown('<h1 class="header-text; color: #ff0000;">PRONE TO MISTAKES! PLEASE VERIFY FROM OTHER SOURCES!!!<br></h1>',unsafe_allow_html=True)
 
     # TAB 1: Analyze Headline
     with tab1:
@@ -687,6 +687,7 @@ def main():
 if __name__ == "__main__":
 
     main()
+
 
 
 
